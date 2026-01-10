8269311bcd1c4c74d90d492e16e92c698a308039
WiktorStaniszewski <wiktorst.8@gmail.com>
2026-01-10 16:48:14 +0100

    CSS

--- js/modules/shadowGrid.js
+++ js/modules/shadowGrid.js
@@ -1,6 +1,6 @@
 
 export function initShadowRecipeGrid(hostEl, {
-  cssUrl = "./css/shadowGrid.css",
+  cssUrl = new URL("../../css/shadowGrid.css", import.meta.url).href,
   placeholderHtml = '<p class="placeholder-text">Write an ingredient or select a random recipe!</p>',
 } = {}) {
   if (!hostEl) throw new Error("initShadowRecipeGrid: hostEl is required");
